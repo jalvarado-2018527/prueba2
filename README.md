a9be81aa3cc465b7161a335927bcd3893ca582f5
jairo <informatica@ALUMNOS.local>
2023-01-18 14:52:16 -0600

    primer commi

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mi primer repositorio en internet 
+
+saludos a todos gente :3
---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function sumar(){
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+    
+    let suma = parseInt(numero1) + parseInt(numero2);
+    console.log(suma);
+}
+
